c60174f5b5b6568f0ac4dde6b48e65196edac212
Diogo Pinto <diogosmp@gmail.com>
2023-08-14 22:36:30 -0700

    Finished implementing the game's logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'dart:math';
 import 'prompt.dart';
 import 'control.dart';
 import 'score.dart';
@@ -36,7 +37,7 @@ class _GamePageState extends State<GamePage> {
   @override
   void initState() {
     super.initState();
-    _model = GameModel(50);
+    _model = GameModel(_newTargetValue());
   }
 
   @override
@@ -46,35 +47,90 @@ class _GamePageState extends State<GamePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Prompt(targetValue: 100),
+            Prompt(targetValue: _model.target),
             Control(model: _model),
             TextButton(
               onPressed: () {
                 _showAlert(context);
               },
-              child: const Text('Tap me here!',
-                  style: TextStyle(color: Colors.blue)),
+              child:
+                  const Text('HIT ME!', style: TextStyle(color: Colors.blue)),
             ),
-            Score(totalScore: _model.totalScore, round: _model.round),
+            Score(
+              totalScore: _model.totalScore,
+              round: _model.round,
+              onStartOver: _startNewGame,
+            ),
           ],
         ),
       ),
     );
   }
 
+  int _pointsForCurrentRound() {
+    const maximumScore = 100;
+    var pointsDifference = _differenceAmount();
+    return maximumScore - pointsDifference + _bonusPoints();
+  }
+
+  String _alertTitle() {
+    String title;
+    var pointsDifference = _differenceAmount();
+    if (pointsDifference == 0) {
+      title = 'Perfect! Extra 100 points!';
+    } else if (pointsDifference == 1) {
+      title = 'You almost had it! Extra 50 points!';
+    } else if (pointsDifference < 5) {
+      title = 'You almost had it!';
+    } else if (pointsDifference <= 10) {
+      title = 'Not bad.';
+    } else {
+      title = 'Are you even trying?';
+    }
+    return title;
+  }
+
+  int _differenceAmount() => (_model.current - _model.target).abs();
+
+  int _bonusPoints() {
+    var pointsDifference = _differenceAmount();
+    if (pointsDifference == 0) {
+      return 100;
+    } else if (pointsDifference == 1) {
+      return 50;
+    }
+    return 0;
+  }
+
+  int _newTargetValue() => Random().nextInt(100) + 1;
+
+  void _startNewGame() {
+    setState(() {
+      _model.totalScore = GameModel.scoreStart;
+      _model.round = GameModel.roundStart;
+      _model.current = GameModel.sliderStart;
+      _model.target = _newTargetValue();
+    });
+  }
+
   void _showAlert(BuildContext context) {
     var okButton = TextButton(
         onPressed: () {
           Navigator.of(context).pop();
-          print('Dismiss pressed!');
+          setState(() {
+            _model.totalScore += _pointsForCurrentRound();
+            _model.target = _newTargetValue();
+            _model.round++;
+          });
         },
         child: const Text('Dismiss'));
     showDialog(
         context: context,
         builder: (BuildContext context) {
           return AlertDialog(
-            title: const Text('Hello there!'),
-            content: Text('The slider\'s value is ${_model.current}'),
+            title: Text(_alertTitle()),
+            content: Text('The slider\'s value is ${_model.current}.\n'
+                'You scored ${_pointsForCurrentRound()} points this round.'),
             actions: [okButton],
             elevation: 5,
           );
--- lib/score.dart
+++ lib/score.dart
@@ -1,31 +1,41 @@
 import 'package:flutter/material.dart';
 
 class Score extends StatelessWidget {
-  const Score({super.key, required this.totalScore, required this.round});
+  const Score(
+      {super.key,
+      required this.totalScore,
+      required this.round,
+      required this.onStartOver});
 
   final int totalScore;
   final int round;
+  final VoidCallback onStartOver;
+
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        TextButton(onPressed: () {}, child: const Text('Start Over')),
-        const Padding(
-          padding: EdgeInsets.all(8.0),
+        TextButton(
+            onPressed: () {
+              onStartOver();
+            },
+            child: const Text('Start Over')),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
           child: Row(
             children: <Widget>[
-              Text('Score: '),
-              Text('100'),
+              const Text('Score: '),
+              Text('$totalScore'),
             ],
           ),
         ),
-        const Padding(
-          padding: EdgeInsets.all(8.0),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
           child: Row(
             children: [
-              Text('Round: '),
-              Text('1'),
+              const Text('Round: '),
+              Text('$round'),
             ],
           ),
         ),
